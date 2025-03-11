b2eaa58c4ac5fb55dd7de1e752a09604d79c1aa3
Nightwatcher85 <gregoryjepsen@gmail.com>
2025-03-11 19:30:26 -0400

    Saved local changes before rebase

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# nextjs.tutorial
-# Created by Greg J
-# This is a code with Mosh to create a issue log.
+>>Test
---
+++ app/components/ProductCards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+
+const ProductCards = () => {
+  return (
+    <div >
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCards
